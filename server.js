a3923c7f847559240da10b73265972f3be107467
Eduardo555 <eduardospillereanzolin@gmail.com>
2019-05-20 22:22:13 -0300

    Modificação do projeto

---
+++ server.js
@@ -0,0 +1,20 @@
+const app =  require('express')()
+const http = require('http').createServer(app)
+
+
+const io = require('socket.io')(http)
+
+// Configura a route.
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname+ '/index.html')
+})
+
+// Para exibir quando umusuario conectar.
+io.on('connection', (socket)=>{
+    // Exibe o id do usuario conectado ao socket.
+    console.log('Nova coneção', socket.id)
+})
+
+http.listen(3000, function(){
+    console.log('Disponivel na porta 3000');
+})
